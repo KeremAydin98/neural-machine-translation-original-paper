5c98d53e6f214431e6ec8a9df30b0afc66134e5e
KeremAydin98 <aydinke16@itu.edu.tr>
2022-09-21 10:21:16 +0300

    initialized models

---
+++ models.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+class RNNsearch(tf.keras.model):
+    def __init__(self):
+
+        super().__init__()
+
+    def call(self,input):
+
+
+class RNNencdec(tf.keras.model):
+    def __init__(self):
+        super().__init__()
+    def call(self, input):
